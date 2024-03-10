8caf403da3c1a8e4f5da26188bd1e67ab5222734
Gareth Larkan <gareth@cis.za.net>
2024-03-10 15:55:46 +0200

    Added the finiliza reuqest function

--- Contracts/ControlledERC721Transfer.sol
+++ Contracts/ControlledERC721Transfer.sol
@@ -2,33 +2,57 @@
 pragma solidity ^0.8.20;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "./IControlledERC721Transfer.sol";
 
-abstract contract ControlledERC721Transfer is ERC721 {
+abstract contract ControlledERC721Transfer is IControlledERC721Transfer {
     
     struct TransferRequest {
         uint256 tokenId;
-        uint256 expiryDate;
+        uint256 requestEndTime;
         address from;
         address to;
         bool approved;
     }
 
+    // The length a transfer on a specific tokenID is valid for.
+    uint256 private transferWindow;
+
+    address private owner;
+
     mapping(uint256 token => TransferRequest request) private transferRequests;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
 
     function submitTransferRequest(
         address _to, 
         uint256 _tokenId
-    ) external {
-        require(msg.sender == ownerOf(_tokenId), "Not token owner");
-        require(block.timestamp > transferRequests[_tokenId].expiryDate, "Transfer request currently active");
+    ) public virtual {
+        require(block.timestamp > transferRequests[_tokenId].requestEndTime, "Transfer request currently active");
         
         transferRequests[_tokenId] = TransferRequest({
             tokenId: _tokenId,
-            expiryDate: block.timestamp + transferWindowPeriod,
+            requestEndTime: 1,
             from: msg.sender,
             to: _to,
             approved: false
         });
     }
 
+    function finalizeRequest(uint256 _tokenId, bool _decision) public virtual {
+        TransferRequest memory request = transferRequests[_tokenId];
+        if(msg.sender != owner) {
+            revert OnlyOwnerFunction(msg.sender);
+        }
+
+        if(request.requestEndTime == 0) {
+            revert NonExistentRequest();
+        }
+
+        if(block.timestamp > request.requestEndTime)
+
+        request.requestEndTime = block.timestamp + transferWindow;
+        request.approved = _decision;
+    }
 }
---
+++ Contracts/IControlledERC721Transfer.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Required interface of an ERC-721 compliant contract.
+ */
+interface IControlledERC721Transfer {
+    
+    error OnlyOwnerFunction(address caller);
+
+    error NonExistentRequest();
+}
